ba6235798cd59061e2b8690b7a1f14972588225a
Yaakov Esral <yaakovez@gmail.com>
2022-07-19 13:07:10 -0400

    merge

--- routes/index.js
+++ routes/index.js
@@ -3,13 +3,18 @@ var router = express.Router();
 
 // quick start: https://www.mongodb.com/docs/drivers/node/current/quick-start/
 
+
+
+
 /* GET home page. */
 router.get('/', function (req, res, next) {
-    res.render('index', { title: 'Express' });
+    const mongoDB = require('../mongoConnection');
+    await mongoDB.connectToServer();
+    const quotesArray = await mongoDB.getCollection().find().toArray();
+    res.render('index', { quotes: quotesArray, title: 'Quotes App' });
 });
 
 router.get('/quotes', async (req, res, next) => {
-
     const mongoDB = require('../mongoConnection');
     await mongoDB.connectToServer();
     const quotesArray = await mongoDB.getCollection().find().toArray();
