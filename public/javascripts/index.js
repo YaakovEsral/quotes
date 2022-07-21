12d50fd00d2d2df77dc530b1882c8dca36e88806
Yaakov Esral <yaakovez@gmail.com>
2022-07-21 19:28:11 -0400

    commented code and minor changes

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -3,6 +3,7 @@ document.addEventListener('click', (e) => {
         toggleFullScreen()
     }
 })
+
 var animationActive = false;
 var originalQuotesArray = [];
 var quotesSelectionArray = [];
@@ -33,6 +34,7 @@ function toggleFullScreen() {
         !document.msFullScreenElement &&
         !document.webkitFullScreenElement) {
 
+        // full screen is done on the container, so that we can switch between quotes and video
         container.requestFullscreen();
         startQuotesAnimation();
         setTimeout(playVideo, videoInterval);
@@ -47,8 +49,8 @@ function toggleFullScreen() {
 
 function startQuotesAnimation() {
     animationActive = true;
-    animate(true);
-    animate(false);
+    animate(true); // main quote
+    animate(false); // secondary quotes
     setInterval(() => toggleDarkMode(quotesDisplay), 210000)
 }
 
@@ -58,6 +60,7 @@ async function animate(main) {
     await sleep(1000);
 
     // start a sequence of transitions
+        /*  note: The sequence of transitions was implemented because it seemed logical. In retrospect, it can probably be done without sequences and just have them execute one at a time, just like is being done for the main quote. Something to keep in mind in case we ever rewrite this program. */
     // const numberOfTransitions = randomNumber(4, 6, true);
     const numberOfTransitions = main ? 1 : randomNumber(6, 8, true);
     for (let i = 1; i <= numberOfTransitions; i++) {
@@ -87,8 +90,6 @@ async function animate(main) {
         // pause between quote transitions
         await sleep(randomNumber(2000, 4000));
     }
-
-
 }
 
 function getQuoteText(main) {
@@ -98,11 +99,12 @@ function getQuoteText(main) {
         // console.log('refilling array');
     }
     
+    // For the main quote, first use entries in recentQuotesArray
     if(recentQuotesArray.length && main) {
         return recentQuotesArray.shift()
     }
 
-    // pick a random quote from the unused array
+    // else, pick a random quote from the unused array
     const index = randomNumber(0, quotesSelectionArray.length, true);
     // console.log(index, quotesSelectionArray);
     return quotesSelectionArray.splice(index, 1)[0];
@@ -134,11 +136,13 @@ async function placeQuoteDiv(div, quote) {
 
     // place a single div
     while (overlap) {
+        // only search if animation is active
         if (!animationActive) {
             console.log('ending');
             break;
         }
 
+        // after too many attempts, clear the screen for the main quote and wait it out for secondary quotes
         if (counter++ > 1000) {
             console.log('COUNTER OVERLOAD');
             if (currentDiv === mainQuoteDiv) {
@@ -162,16 +166,19 @@ async function placeQuoteDiv(div, quote) {
         // place the div
         currentDiv.style.left = `${left}px`;
         currentDiv.style.top = `${top}px`;
-        // used this for testing - each new placement attempt
-        // await sleep(1000)
+        
+        // await sleep(1000) // used this for testing - each new placement attempt
 
         // check that this div doesn't overlap with a different one
         overlap = checkOverlap(currentDiv, allQuoteDivs);
         // console.log('overlap', overlap, 'hidden', currentDiv.classList.contains('hidden'));
     }
+    // once there is no overlap, show the div
     show(currentDiv);
     currentlyDisplayedDivs.push(currentDiv);
     // console.log('quote appeared', currentlyDisplayedDivs);
+
+    // hide div after a timeout
     const timeout = currentDiv === mainQuoteDiv ? mainQuoteInterval : randomNumber(minimumSequenceInterval, maximumSequenceInterval);
     // setTimeout(() => hide(currentDiv), timeout);
     setTimeout(() => {
@@ -186,10 +193,6 @@ async function placeQuoteDiv(div, quote) {
 
 function removeDivs() {
     console.log('removing divs');
-    // allQuoteDivs.forEach(div => {
-    //     div.style.left = '-100%';
-    //     div.style.top = '-100%';
-    // })
-    currentlyDisplayedDivs.forEach(hide);
+    currentlyDisplayedDivs.forEach(hide); // as long as they are hidden, they aren't a problem with overlapping
     // currentlyDisplayedDivs.length = 0; // ### bug - we should be able to empty the array here, but it seems to cause problems
 }
